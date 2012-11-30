a31feb7d96e77c8ba312401829efc2498958a04d
Erik Vos <erikjan.vos@nelen-schuurmans.nl>
2012-11-30 10:24:51 +0000

    properly set dependency versions

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ long_description = '\n\n'.join([
 install_requires = [
     'Django',
     'django-staticfiles',
-    'lizard-map >= 4.0',
-    'lizard-ui >= 4.0',
+    'lizard-map >= 4.0, < 5.0',
+    'lizard-ui >= 4.0, < 5.0',
     'django-nose',
     ],
 
